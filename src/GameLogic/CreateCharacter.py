5fa0a46d1601c6bf8e44269a04a366b7d2f9a96d
root <root@csx20194.cloud.canlab.ibm.com>
2017-12-02 12:37:51 -0500

    character stat selection

--- src/GameLogic/CreateCharacter.py
+++ src/GameLogic/CreateCharacter.py
@@ -6,10 +6,69 @@ class CreateCharacter(GenericGameLogic):
     dict characterstats
     def __init__(self,channel):
         super(self, channel)
-
         
 
         
 #    def messageReceive(self,message):
         
-    def         
+    def createCharacter(race,occupation):
+        hp = 120
+        spd = 15
+        attk = 10
+        mp = 15
+        crt = 2
+
+        if race == 'elf': 
+            spd += 10
+            attk += 5
+            mp +=10
+            crt +=2
+       
+        elif race == 'dwarf'
+            spd -= 5
+            hp += 90
+            mp -= 5
+
+        elif race == 'troll'
+            hp += 90
+            attk += 10
+            spd -= 10
+            mp -=15
+            
+        elif race == 'gnome'
+            hp -=20
+            spd +=20
+            attk += 5
+            crt+=5
+        elif race == 'human'
+            hp += 20
+            attk +=10
+            mp +=5
+            spd +=5
+            
+        if occupation == 'thief'
+            spd += 10
+            hp -=10
+            attk += 5
+            crt +=1
+
+        elif occupation == 'smith'
+            hp += 20
+        
+        elif occupation == 'drunk'
+            attk -= 4
+            crt += 4
+        elif occupation == 'librarian'
+            hp += 5
+            mp += 10
+
+        elif occupation = 'hunter'
+            attk += 5
+            crt +=5
+
+        
+
+
+    def createChar(hp,spd,attk,mp,crt):
+        #characterstats{ health: 15,speed: 15, attack: 15, mana: 15, crit: 2}
+        #create char
