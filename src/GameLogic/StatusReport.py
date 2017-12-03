62906d9307c6e96e8f21f064727d6e6350120b94
Noah Weninger <nweninge@ualberta.ca>
2017-12-02 19:38:13 -0500

    Print room

--- src/GameLogic/StatusReport.py
+++ src/GameLogic/StatusReport.py
@@ -6,7 +6,9 @@ class StatusReport(GenericGameLogic):
         super().__init__(print_method, data)
 
     async def print_current_statuses(self, message):
-        await self.printMethod(message.channel, "Your environment: \n{}"
-                               .format("".join(["name: {}\ndescription: {}\nhealth: {}\n-------------------\n"
+        await self.printMethod(message.channel, "Your environment: \nYou are in {}\n{}"
+                               .format(
+                                   self.data.current_room.describe(),
+                                   "".join(["-------------------\nname: {}\ndescription: {}\nhealth: {}\n"
                                                .format(i.name, i.description, i.health)
                                                 for i in self.data.current_scenario])))
